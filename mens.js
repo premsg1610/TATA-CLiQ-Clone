d5525597ac736d75b8c45007ef1976a04e9abe46
Roka24 <sunilroka@gmail.com>
2022-03-30 15:21:01 +0530

    menspage

---
+++ mens.js
@@ -0,0 +1,164 @@
+var mensData =  [
+    
+    
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQM4QSXArzo_bT60_k8SbkSfGzca9CJdZswRTf3ES_-a_ViuhXb" ,
+        nam : "Red Chief" ,
+        namefull: "Red Chief Men's Tan Casual Shoes" ,
+        price :  "₹3695" ,
+        rating : "4.3"
+
+    },
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT_tflt0NXsNJB-hIe5csBzkfbEwq6RguAvnZ3PdxQwSyYl9Zjv" ,
+        nam : "SOLEPLAY",
+        namefull: "SOLEPLAY by Westside Beige Rainwear Loafers" ,
+        price :  " ₹799" ,
+        rating : " 4.4 (8)"
+
+    },
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkh0HEtOe_tqQ37NEKoTSmTBaBvU6oE6i9doPgX81P1my0k3WG" ,
+        nam : "Mochi" ,
+        namefull: " Mochi Men's Tan Casual Loafers" ,
+        price :  " ₹1743" ,
+        rating : "4 (1)"
+
+    },
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRVffrsQ4iukzT_w2_NArCTSOQSiDW8_I299Cq9sC8iLEDaha5W" ,
+        nam : "U.S. Polo Assn." ,
+        namefull: "  U.S. Polo Assn. Men's Quebec Black Loafers" ,
+        price :  " ₹2398" ,
+        rating : "5(1)"
+
+    },
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR7rNRP24-vLHouMGE5xwghoDfcdjcI7UzH5xUhcyui0zvW00ka" ,
+        nam : "SOLEPLAY",
+        namefull: " SOLEPLAY by Westside Indigo Perforated Loafers" ,
+        price :  " ₹799" ,
+        rating : "3.8(147)"
+
+    },
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6BWNrJeQM76d7VWo_x7UJ-tuFrz9r4KdzIzaHLL58W43GQj7I" ,
+        nam : "Florsheim" ,
+        namefull: "  Florsheim Brown Casual Loafers" ,
+        price :  "₹1996" ,
+        rating : "4.6(5)"
+
+    },
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdfEQ1dFiIFm0GPUZm9athuWHVwYktebUrgLX0N-q7najoWMAE" ,
+        nam : "Ruosh" ,
+        namefull: "  Ruosh Men's Tan Casual Loafers" ,
+        price :  " ₹4990" ,
+        rating : "4"
+
+    },
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTVdUVoDQP9r_K4yk8kWiVmDuK_8kGs-uo6O7ADNd6reK2OQm0" ,
+        nam : "Skechers" ,
+        namefull: "  Skechers Men's FLEX ADVANTAGE Casual Shoe Slip-Ons" ,
+        price :  "₹2400" ,
+        rating : "3.9(21)"
+
+    },
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-K-jPeUnUeEX7L7qRQtlYMtP1P96l-1waaIJIuvx5ekb5qBCV" ,
+        nam : "Alberto Torresi" ,
+        namefull: "  Alberto Torresi Men's Calabria Tan Casual Loafers" ,
+        price :  " ₹1999" ,
+        rating : "3.1(10)"
+
+    },
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS0q7FWVm4nNFkDP8_dBfO4zrR-dI4kOo5_kTtDqKEwWFhs8Ng" ,
+        nam : "Woodland" ,
+        namefull: "  Woodland Men's Khaki Casual Shoes" ,
+        price :  "  ₹2577" ,
+        rating : "3.8(4)"
+
+    },
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRmQWrMcaEiIRM81V2FUZTClR0GcpmlIkZXLCYS3gmzXvYneKs7" ,
+        nam : "Clarks" ,
+        namefull: "  Clarks Men's Black Boat Shoes" ,
+        price :  "₹3899" ,
+        rating : "3"
+
+    },
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ41ptvgSL2oU_IiaenyZqyFZjOUFYW3PgWsENK9UPSVaE8eCuq" ,
+        nam : "ID" ,
+        namefull: " ID Men's Black Casual Loafers" ,
+        price :  " ₹1350" ,
+        rating : "3.9(13)"
+
+    },
+    {  image_url : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSsT2WO0_Gvy3aR91PSh3BXMPArhHueJznT8aiPuYZGYGolJvo8" ,
+        nam : "Red Tape" ,
+        namefull: " Red tape Men's Black Casual Loafers" ,
+        price :  "  ₹1474" ,
+        rating : "3.5(81)"
+
+    },
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw-sutB6uGb-CTihb6P0ZOT__sdjUPYGwXE9Q2DSlKWVDaKNR_" ,
+        nam : "San Frissco" ,
+        namefull: " San Frissco Men's Carbon Black Casual Loafers" ,
+        price :  "  ₹1848" ,
+        rating : "5(4)"
+
+    },
+    
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTldf19HBnt4W8lfDak1C3Mqkta6scPuxLgoH0jlb5GQOonwFcc" ,
+        nam : "Ruosh" ,
+        namefull: " Ruosh Men's Tan Casual Loafers" ,
+        price :  "  ₹3990" ,
+        rating : "4(5)"
+
+    },
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQaMUuy2V7OwBOo55juD8C2zHu_fwKOqGw-mmqhvjEV2MlsUZN8" ,
+        nam : " Alberto Torresi" ,
+        namefull: " Alberto Torresi Men's Men's Maroon Casual Slip-Ons" ,
+        price :  "₹1799" ,
+        rating : "4(9)"
+
+    },
+   
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTkGhBtNfXOEDT5TZsl_6PY5FQqOKZkaorbmY3iKYmjMUmOYWfE" ,
+        nam : "Aldo" ,
+        namefull: " Aldo Men's Cognac Casual Loafers" ,
+        price :  " ₹11199" ,
+        rating : ""
+
+    },
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTKLeBDWcqR_WxpMclqEyDzzad0Jf9JhZ0LeX61dOnnrKG5E3Zk" ,
+        nam : "Tresmode " ,
+        namefull: " Tresmode Men's Brown Casual Loafer" ,
+        price :  " ₹4500" ,
+        rating : ""
+
+    },
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTKqx7VnSA_kqzfX-AKB_R8p2cchYqD9rsRbMGWUHEbSvQ3hiz_" ,
+        nam : "San Frissco " ,
+        namefull: " San Frissco Black Mule Shoes" ,
+        price :  "₹1698" ,
+        rating : " 4.4(9)"
+
+    },
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQmPyjVMBvcZWXX08PSnED6e3IKT_QJeYhyZ07_X-tqA76qI2-A" ,
+        nam : "Lee Cooper" ,
+        namefull: "  Lee Cooper Men's CORE EVOLUTION Tan Loafers" ,
+        price :  "   ₹2549" ,
+        rating : ""
+    },
+    ];
+
+mensData.map(function(ele){
+    var box = document.createElement("div");
+    var img1 = document.createElement("img")
+    img1.src=ele.image_url 
+    img1.setAttribute
+    var Productname = document.createElement("h3")
+    Productname.innerText = ele.nam
+    var fullname = document.createElement("p")
+    fullname.innerText = ele.namefull
+    var cost = document.createElement("h3")
+    cost.innerText = ele.price
+    var ratings = document.createElement("p")
+    ratings.innerText = ele.rating
+
+    box.append(img1,Productname,fullname,cost,ratings)
+
+    document.querySelector("#container").append(box)
+})
