fdfc79406e28cb823c2a73525b98bc26961969e2
premsg1610 <premsgupta1997@gmail.com>
2022-03-31 00:44:26 +0530

    price updated

--- mens.js
+++ mens.js
@@ -4,7 +4,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQM4QSXArzo_bT60_k8SbkSfGzca9CJdZswRTf3ES_-a_ViuhXb" ,
         nam : "Red Chief" ,
         namefull: "Red Chief Men's Tan Casual Shoes" ,
-        price :  "₹3695" ,
+        price :  "3695" ,
         off : "₹4695",
         rating : "4.3"
 
@@ -12,7 +12,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT_tflt0NXsNJB-hIe5csBzkfbEwq6RguAvnZ3PdxQwSyYl9Zjv" ,
         nam : "SOLEPLAY",
         namefull: "SOLEPLAY by Westside Beige Rainwear Loafers" ,
-        price :  " ₹799" ,
+        price :  "799" ,
         off :  " ₹999" ,
         rating : " 4.4 (8)"
 
@@ -20,7 +20,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkh0HEtOe_tqQ37NEKoTSmTBaBvU6oE6i9doPgX81P1my0k3WG" ,
         nam : "Mochi" ,
         namefull: " Mochi Men's Tan Casual Loafers" ,
-        price :  " ₹1743" ,
+        price :  "1743" ,
         off :  " ₹2743" ,
         rating : "4 (1)"
 
@@ -28,7 +28,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRVffrsQ4iukzT_w2_NArCTSOQSiDW8_I299Cq9sC8iLEDaha5W" ,
         nam : "U.S. Polo Assn." ,
         namefull: "  U.S. Polo Assn. Men's Quebec Black Loafers" ,
-        price :  " ₹2398" ,
+        price :  "2398" ,
         off :  " ₹3998" ,
         rating : "5(1)"
 
@@ -36,7 +36,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR7rNRP24-vLHouMGE5xwghoDfcdjcI7UzH5xUhcyui0zvW00ka" ,
         nam : "SOLEPLAY",
         namefull: " SOLEPLAY by Westside Indigo Perforated Loafers" ,
-        price :  " ₹799" ,
+        price :  "799" ,
         off :  " ₹1199" ,
         rating : "3.8(147)"
 
@@ -44,7 +44,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6BWNrJeQM76d7VWo_x7UJ-tuFrz9r4KdzIzaHLL58W43GQj7I" ,
         nam : "Florsheim" ,
         namefull: "  Florsheim Brown Casual Loafers" ,
-        price :  "₹1996" ,
+        price :  "1996" ,
         off :  "₹2996" ,
         rating : "4.6(5)"
 
@@ -52,7 +52,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdfEQ1dFiIFm0GPUZm9athuWHVwYktebUrgLX0N-q7najoWMAE" ,
         nam : "Ruosh" ,
         namefull: "  Ruosh Men's Tan Casual Loafers" ,
-        price :  " ₹4990" ,
+        price :  "4990" ,
         off :  " ₹6990" ,
         rating : "4"
 
@@ -60,7 +60,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTVdUVoDQP9r_K4yk8kWiVmDuK_8kGs-uo6O7ADNd6reK2OQm0" ,
         nam : "Skechers" ,
         namefull: "  Skechers Men's FLEX ADVANTAGE Casual Shoe Slip-Ons" ,
-        price :  "₹2400" ,
+        price :  "2400" ,
         off :  "₹3400" ,
         rating : "3.9(21)"
 
@@ -68,7 +68,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-K-jPeUnUeEX7L7qRQtlYMtP1P96l-1waaIJIuvx5ekb5qBCV" ,
         nam : "Alberto Torresi" ,
         namefull: "  Alberto Torresi Men's Calabria Tan Casual Loafers" ,
-        price :  " ₹1999" ,
+        price :  "1999" ,
         off :  " ₹2999" ,
         rating : "3.1(10)"
 
@@ -76,7 +76,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS0q7FWVm4nNFkDP8_dBfO4zrR-dI4kOo5_kTtDqKEwWFhs8Ng" ,
         nam : "Woodland" ,
         namefull: "  Woodland Men's Khaki Casual Shoes" ,
-        price :  "  ₹2577" ,
+        price :  "2577" ,
         off :  "  ₹4577" ,
         rating : "3.8(4)"
 
@@ -84,7 +84,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRmQWrMcaEiIRM81V2FUZTClR0GcpmlIkZXLCYS3gmzXvYneKs7" ,
         nam : "Clarks" ,
         namefull: "  Clarks Men's Black Boat Shoes" ,
-        price :  "₹3899" ,
+        price :  "3899" ,
         off :  "₹4899" ,
         rating : "3"
 
@@ -92,7 +92,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ41ptvgSL2oU_IiaenyZqyFZjOUFYW3PgWsENK9UPSVaE8eCuq" ,
         nam : "ID" ,
         namefull: " ID Men's Black Casual Loafers" ,
-        price :  " ₹1350" ,
+        price :  "1350" ,
         off :  " ₹3350" ,
         rating : "3.9(13)"
 
@@ -100,7 +100,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSsT2WO0_Gvy3aR91PSh3BXMPArhHueJznT8aiPuYZGYGolJvo8" ,
         nam : "Red Tape" ,
         namefull: " Red tape Men's Black Casual Loafers" ,
-        price :  "  ₹1474" ,
+        price :  "1474" ,
         off :  "  ₹3474" ,
         rating : "3.5(81)"
 
@@ -108,7 +108,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw-sutB6uGb-CTihb6P0ZOT__sdjUPYGwXE9Q2DSlKWVDaKNR_" ,
         nam : "San Frissco" ,
         namefull: " San Frissco Men's Carbon Black Casual Loafers" ,
-        price :  "  ₹1848" ,
+        price :  "1848" ,
         off :  "  ₹3848" ,
         rating : "5(4)"
 
@@ -117,7 +117,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTldf19HBnt4W8lfDak1C3Mqkta6scPuxLgoH0jlb5GQOonwFcc" ,
         nam : "Ruosh" ,
         namefull: " Ruosh Men's Tan Casual Loafers" ,
-        price :  "  ₹3990" ,
+        price :  "3990" ,
         off :  "  ₹5990" ,
         rating : "4(5)"
 
@@ -125,7 +125,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQaMUuy2V7OwBOo55juD8C2zHu_fwKOqGw-mmqhvjEV2MlsUZN8" ,
         nam : " Alberto Torresi" ,
         namefull: " Alberto Torresi Men's Men's Maroon Casual Slip-Ons" ,
-        price :  "₹1799" ,
+        price :  "1799" ,
         off :  "₹3799" ,
         rating : "4(5)"
 
@@ -134,7 +134,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTkGhBtNfXOEDT5TZsl_6PY5FQqOKZkaorbmY3iKYmjMUmOYWfE" ,
         nam : "Aldo" ,
         namefull: " Aldo Men's Cognac Casual Loafers" ,
-        price :  " ₹11199" ,
+        price :  "11199" ,
         off :  " ₹13199" ,
         rating : ""
 
@@ -142,7 +142,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTKLeBDWcqR_WxpMclqEyDzzad0Jf9JhZ0LeX61dOnnrKG5E3Zk" ,
         nam : "Tresmode " ,
         namefull: " Tresmode Men's Brown Casual Loafer" ,
-        price :  " ₹4500" ,
+        price :  "4500" ,
         off :  " ₹6500" ,
         rating : ""
 
@@ -150,7 +150,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTKqx7VnSA_kqzfX-AKB_R8p2cchYqD9rsRbMGWUHEbSvQ3hiz_" ,
         nam : "San Frissco " ,
         namefull: " San Frissco Black Mule Shoes" ,
-        price :  "₹1698" ,
+        price :  "1698" ,
         off :  "₹2698" ,
         rating : " 4.4(9)"
 
@@ -158,7 +158,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQmPyjVMBvcZWXX08PSnED6e3IKT_QJeYhyZ07_X-tqA76qI2-A" ,
         nam : "Lee Cooper" ,
         namefull: "  Lee Cooper Men's CORE EVOLUTION Tan Loafers" ,
-        price :  "   ₹2549" ,
+        price :  "2549" ,
         off :  "   ₹4549" ,
         rating : ""
     },
@@ -166,7 +166,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSelKUzPIhZYrwGfNQ23BaLLl1DYSRx7cHqBPQtmXxfHn-Suh4u" ,
         nam : "Hush Puppies" ,
         namefull: "  Hush Puppies by Bata Men's Tan Casual Loafers" ,
-        price :  "    ₹3399" ,
+        price :  "3399" ,
         off :  "   ₹3999" ,
         rating : ""
     },
@@ -174,14 +174,14 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQneEYMuUFF8InOLE9r1aiNjl6BG4xtBIVMb4PuLByKzqNXa2vz" ,
         nam : "Woodland" ,
         namefull: " Woodland Men's Tobacco Casual Slip-Ons" ,
-        price :  "    ₹2621" ,
+        price :  "2621" ,
         off :  "   ₹3495" ,
         rating : "3.3(9)"
     },
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRo1ryKGdk6eUKBEgt9fIh-TwAaYzii8DE7L1bfaNJy-D2Pqsk9" ,
         nam : "Bata" ,
         namefull: "Bata Men's Black Casual Slip-Ons" ,
-        price :  "    ₹906" ,
+        price :  "906" ,
         off :  "   ₹1066" ,
         rating : "4(1)"
     },
@@ -189,7 +189,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTe2g0WFZjfABt9VyQpJ6bf9E9y6G-fDmhNanZYxkkSvLmiu1tf" ,
         nam : "Pravers England" ,
         namefull: " Pavers England Men's Navy Plimsolls" ,
-        price :  "    ₹4999" ,
+        price :  "4999" ,
         off :  "" ,
         rating : "5(1)"
     },
@@ -197,7 +197,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ45SXSupNjZnd5EYgOZk5b1tCnggvSYidXkQz2H6OijGlq-jWK" ,
         nam : "Woodland" ,
         namefull: "Woodland Men's Brown Casual Slip-Ons " ,
-        price :  "₹2795" ,
+        price :  "2795" ,
         off :  "" ,
         rating : ""
     },
@@ -205,7 +205,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSEIhjFXsTMyAS8HR9FTDEacBCM40V9UE0AD-ip91VU_7A5mrDK" ,
         nam : "Sparx" ,
         namefull: "Sparx Black Casual Plimsolls" ,
-        price :  "₹499" ,
+        price :  "499" ,
         off :  "" ,
         rating : "4.2(74)"
     },
@@ -213,7 +213,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR2fsCb4KNNlGxu1TRytLgUD8ysZJobGC0dsL_Bx4v-6yFHTjXn" ,
         nam : "Bugatti" ,
         namefull: "Bugatti Men's Chocolate Brown Mule Shoes" ,
-        price :  "₹7499" ,
+        price :  "7499" ,
         off :  "" ,
         rating : ""
     },
@@ -221,7 +221,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQukuJF96G_VYofy7PRwSG-MP9a9kkcADyZuoDOxLTTqtaEh97F" ,
         nam : "Red Chief" ,
         namefull: "Red Chief Men's Navy/Blue Casual Shoes" ,
-        price :  "    ₹1999" ,
+        price :  "1999" ,
         off :  "   ₹3999" ,
         rating : "4.2(152)"
     },
@@ -229,7 +229,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTWv_MhHBRmEx7G_OMtuN0Gk85Ed7iobRb4sto3G_UCcyd-s_qy" ,
         nam : "Woodland" ,
         namefull: "Sparx Black Casual Plimsolls" ,
-        price :  "    ₹521" ,
+        price :  "521" ,
         off :  "   ₹549" ,
         rating : "3.9(54)"
     },
@@ -237,7 +237,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnRm2Da7udA1PKkS6X09mW_AjLvxFwMjvX9zsXiCsDht5zdIZf" ,
         nam : "Puma" ,
         namefull: "Puma Men's Unisex Lazy Knit Peacoat Slip-Ons" ,
-        price :  "    ₹1949" ,
+        price :  "1949" ,
         off :  "   ₹2999" ,
         rating : "4(53)"
     },
@@ -245,7 +245,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTBUvpc9EdYJy1MXg3oGVDqYRo5PKa3Fr5ibC04ioSGWFx5Ddkm" ,
         nam : "Pavers England" ,
         namefull: "Pavers England Men's Brown Plimsolls" ,
-        price :  "    ₹4999" ,
+        price :  "4999" ,
         off :  "" ,
         rating : "5(1)"
     },
@@ -253,7 +253,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQgP55WsnK-5iCYz47T4KfsC_K-IytrzcZuhF_df4cgt1eFNsCO" ,
         nam : "Red Chief" ,
         namefull: "Red Chief Men's Tan Casual Slip-Ons" ,
-        price :  "    ₹2579" ,
+        price :  "2579" ,
         off :  "   ₹4299" ,
         rating : "4(19)"
     },
@@ -261,7 +261,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSybd27cQTfaLXBvSo6rZvMAjheZ8vrYv5Nqn4Vz8_A8DwDXfiu" ,
         nam : "Allen Solly" ,
         namefull: " Allen Solly Men's Brown Casual Loafers" ,
-        price :  "    ₹2079" ,
+        price :  "2079" ,
         off :  "   ₹3199" ,
         rating : ""
     },
@@ -269,7 +269,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ9laOfmdB_95Lxf5we0qmMTz8qcxFjlL6KIfX0IVEPTc2_HCJg" ,
         nam : "Bugatti" ,
         namefull: "Bugatti Men's Navy Casual Slip-Ons" ,
-        price :  "₹8499" ,
+        price :  "8499" ,
         off :  "" ,
         rating : ""
     },
@@ -277,7 +277,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq-AQVdu1ByU07LjntyktxGqoB9jAZ_eBCA9_-2UV158TNkn_B" ,
         nam : "Pavers England" ,
         namefull: "Pavers England Men's Black Fisherman Sandals" ,
-        price :  "₹3999" ,
+        price :  "3999" ,
         off :  "" ,
         rating : "4.7(3)"
     },
@@ -285,7 +285,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRoteYJ7l6oeeFB4b0BN50zgjzbfx4Q0Y0rnsPyp-kS3B6iZSQc" ,
         nam : "Arrow" ,
         namefull: "Arrow Men's Carmine Tan Slip-Ons" ,
-        price :  "    ₹2598" ,
+        price :  "2598" ,
         off :  "   ₹3999" ,
         rating : "3.7(3)"
     },
@@ -293,7 +293,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6ik-OBf8JGwnJjtm20eFUgWA3-cAa48nm3WMujogt9hcb2haN" ,
         nam : "Allen Cooper" ,
         namefull: "Allen Cooper Men's Brown Casual Loafers" ,
-        price :  "    ₹1760" ,
+        price :  "1760" ,
         off :  "   ₹5499" ,
         rating : "3.3(9)"
     },
@@ -301,7 +301,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQiWwK65ErUK4LDroO74EDXlv_4jbeyAuSn4RKpxFpxxwfB3nsw" ,
         nam : "Red Chief" ,
         namefull: "Red Chief Men's Camel Derby Shoes" ,
-        price :  "    ₹2579" ,
+        price :  "2579" ,
         off :  "   ₹4299" ,
         rating : "4.3(3)"
     },
@@ -309,7 +309,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ7jANyZWv2LYxUudNzZmixet99VoW6vSEB1Y9jnKi-Iey39OaD" ,
         nam : "Allen Cooper" ,
         namefull: "Allen Cooper Men's Black Casual Loafers" ,
-        price :  "    ₹1886" ,
+        price :  "1886" ,
         off :  "   ₹5240" ,
         rating : ""
     },
@@ -317,7 +317,7 @@ var mensData =  [
     {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQomv9FEqPX-7op8oU9Chl-GmnCudUj-zIbJE7KYwKaT3VFLe6V" ,
         nam : "SOLEPLAY" ,
         namefull: "SOLEPLAY by Westside Grey Perforated Plimsolls" ,
-        price :  "    ₹799" ,
+        price :  "799" ,
         off :  "" ,
         rating : "3.9(151)"
     }
--- women.js
+++ women.js
@@ -4,8 +4,8 @@ var womensData = [
         "Images/1.jpeg",
       head: "Utsa",
       name: "Utsa by Westside Greey Printed A-Line Kurti",
-      price: "₹799",
-      strikedoffprice: null,
+      price: "799",
+      strikedoffprice:"",
       rating:"4.6(9)",
     },
     {
@@ -13,7 +13,7 @@ var womensData = [
         "Images/2.jpeg",
       head:"Aurelia",
       name: "Aurelia White Cotton Embroidered Straight Kurta",
-      price: "₹749",
+      price: "749",
       strikedoffprice: "₹1499",
       rating:"4.3(9)",
     },
@@ -22,342 +22,342 @@ var womensData = [
         "Images/3.jpeg",
         head:"Utsa",
         name: "Utsa by Westside Mauve Embroidered A-Line Kurta",
-      price: "₹1299",
-      strikedoffprice: null,
+      price: "1299",
+      strikedoffprice: "",
       rating:"3.6(7)",
     },
     {
       image_url:
-        "/Images/4.jpeg",
+        "Images/4.jpeg",
         head:"W",
         name: "W Minion Yellow Embellished A Line Kurta",
-      price: "₹2499",
+      price: "2499",
       strikedoffprice: "₹4999",
-      rating:"5(1)",
+      rating:"5.0(1)",
     },
     {
       image_url:
-        "/Images/5.jpeg",
+        "Images/5.jpeg",
         head:"Biba",
         name: "Biba Green Printed Straight Kurti",
-      price: "₹1599",
-      strikedoffprice: null,
+      price: "1599",
+      strikedoffprice: "",
       rating:"4.4(5)",
     },
     {
       image_url:
-        "/Images/6.jpeg",
+        "Images/6.jpeg",
         head:"Biba",
         name: "Biba Indigo Blue Cotton Straight Kurta",
-      price: "₹998",
+      price: "998",
       strikedoffprice: "₹1999",
-      rating:null,
+      rating:"",
     },
     {
       image_url:
-        "/Images/7.jpeg",
+        "Images/7.jpeg",
         head:"Aurelia",
         name: "Aurelia Blue Striped Kurta",
-      price: "₹649",
+      price: "649",
       strikedoffprice: "₹1299",
-      rating:"5(2)",
+      rating:"5.0(2)",
     },
     {
       image_url:
-        "/Images/8.jpeg",
+        "Images/8.jpeg",
         head:"Zuba",
         name: "Zuba by Westside Orange Printed Ombre A-line Kurta",
-      price: "₹2599",
-      strikedoffprice: null,
+      price: "2599",
+      strikedoffprice: "",
       rating:"4.1(10)",
     },
     {
       image_url:
-        "/Images/9.jpeg",
+        "Images/9.jpeg",
         head:"Zuba",
         name: "Zuba by Westside Green Floral A-Line Kurta",
-      price: "₹2299",
-      strikedoffprice: null,
+      price: "2299",
+      strikedoffprice: "",
       rating:"4.8(4)",
     },
     {
       image_url:
-        "/Images/10.jpeg",
+        "Images/10.jpeg",
         head:"Diza",
         name: "Diza Curves by Westside Orange Pintuck A-Line Kurta",
-      price: "₹1199",
-      strikedoffprice: null,
+      price: "1199",
+      strikedoffprice: "",
       rating:"4.6(5)",
     },
     {
       image_url:
-        "/Images/11.jpeg",
+        "Images/11.jpeg",
         head:"Fabindia",
         name: "Fabindia Black Cotton Straight Kurta",
-      price: "₹1399",
-      strikedoffprice: null,
-      rating:null,
+      price: "1399",
+      strikedoffprice: "",
+      rating:"",
     },
     {
       image_url:
-        "/Images/12.jpeg",
+        "Images/12.jpeg",
         head:"Diza",
         name: "Diza Curves by Westside Pink Chevron Straight Kurta",
-      price: "₹799",
-      strikedoffprice: null,
-      rating:"4(5)",
+      price: "799",
+      strikedoffprice: "",
+      rating:"4.0(5)",
     },
     {
         image_url:
-          "/Images/13.jpeg",
+          "Images/13.jpeg",
           head:"Fabindia",
           name: "Fabindia Pink Cotton Chequered Straight Kurta",
-        price: "₹1499",
-        strikedoffprice: null,
-        rating:"5(1)",
+        price: "1499",
+        strikedoffprice: "",
+        rating:"5.0(1)",
       },
       {
         image_url:
-          "/Images/14.jpeg",
+          "Images/14.jpeg",
           head:"Global Desi",
           name: "Global Desi Blue Printed Kurta",
-        price: "₹1329",
+        price: "1329",
         strikedoffprice: "₹1899",
-        rating:null,
+        rating:"",
       },
       {
         image_url:
-          "/Images/15.jpeg",
+          "Images/15.jpeg",
           head:"Juniper",
           name: "Juniper Grey Cotton Embroidered A Line Kurta With Mask",
-        price: "₹1039",
+        price: "1039",
         strikedoffprice: "₹1999",
         rating:"4.2(24)",
       },
       {
         image_url:
-          "/Images/16.jpeg",
+          "Images/16.jpeg",
           head:"Juniper",
           name: "Juniper Green Printed Straight Kurta With Mask",
-        price: "₹799",
+        price: "799",
         strikedoffprice: "₹1699",
-        rating:null,
+        rating:"",
       },
       {
         image_url:
-          "/Images/17.jpeg",
+          "Images/17.jpeg",
           head:"Global Desi",
           name: "Global Desi Pink Printed Kurta",
-        price: "₹2099",
-        strikedoffprice: null,
-        rating:null,
+        price: "2099",
+        strikedoffprice: "",
+        rating:"",
       },
       {
         image_url:
-          "/Images/18.jpeg",
+          "Images/18.jpeg",
           head:"Aks",
           name: "Aks Coral Cotton Printed Straight Kurta",
-        price: "₹499",
+        price: "499",
         strikedoffprice: "₹999",
-        rating:null,
+        rating:"",
       },
       {
         image_url:
-          "/Images/19.jpeg",
+          "Images/19.jpeg",
           head:"Anubhutee",
           name: "Anubhutee Blue Printed Straight Kurti",
-        price: "₹584",
+        price: "584",
         strikedoffprice: "₹1299",
         rating:"4.3(106)",
       },
       {
         image_url:
-          "/Images/20.jpeg",
+          "Images/20.jpeg",
           head:"Aks",
           name: "Aks Grey Printed Straight Kurta",
-        price: "₹849",
+        price: "849",
         strikedoffprice: "₹1699",
         rating:"3.5(8)",
       },
       {
         image_url:
-          "/Images/21.jpeg",
+          "Images/21.jpeg",
           head:"Soch",
           name: "Soch Grey & White Embellished A Line Kurta",
-        price: "₹999",
+        price: "999",
         strikedoffprice: "₹1998",
-        rating:null,
+        rating:"",
       },
       {
         image_url:
-          "/Images/22.jpeg",
+          "Images/22.jpeg",
           head:"Jaipur Kurti",
           name: "Jaipur Kurti Yellow Cotton Printed Straight Kurta",
-        price: "₹542",
+        price: "542",
         strikedoffprice: "₹1349",
-        rating:"4(63)",
+        rating:"4.0(63)",
       },
       {
         image_url:
-          "/Images/23.jpeg",
+          "Images/23.jpeg",
           head:"Gerua",
           name: "Gerua Beige & Red Printed A Line Kurti",
-        price: "₹674",
+        price: "674",
         strikedoffprice: "₹1499",
-        rating:"3(1)",
+        rating:"3.0(1)",
       },
       {
         image_url:
-          "/Images/24.jpeg",
+          "Images/24.jpeg",
           head:"Yufta",
           name: "Yufta Women Blue & Offwhite Printed Kaftan Kurta",
-        price: "₹799",
+        price: "799",
         strikedoffprice: "₹1999",
-        rating:"4(3)",
+        rating:"4.0(3)",
       },
       {
         image_url:
-          "/Images/25.jpeg",
+          "Images/25.jpeg",
           head:"Gerua",
           name: "Gerua Yellow Printed Cotton Kurti",
-        price: "₹494",
+        price: "494",
         strikedoffprice: "₹1099",
         rating:"4.1(30)",
       },
       {
         image_url:
-          "/Images/26.jpeg",
+          "Images/26.jpeg",
           head:"Tara-C-Tara",
           name: "Tara-C-Tara Pink Embellished A Line Kurta With Dupatta",
-        price: "₹2759",
+        price: "2759",
         strikedoffprice: "₹4599",
-        rating:null,
+        rating:"",
       },
       {
         image_url:
-          "/Images/27.jpeg",
+          "Images/27.jpeg",
           head: "Tara-C-Tara",
           name: "Tara-C-Tara Light Blue V Neck Anarkali Kurta With Dupatta",
-        price: "₹2579",
+        price: "2579",
         strikedoffprice: "₹4299",
-        rating:"5(1)",
+        rating:"5.0(1)",
       },
       {
         image_url:
-          "/Images/28.jpeg",
+          "Images/28.jpeg",
           head:"Yufta",
           name: "Yufta Multicolor Printed Kaftan Kurti",
-        price: "₹719",
+        price: "719",
         strikedoffprice: "₹1799",
         rating:"4.5(2)",
       },
       {
         image_url:
-          "/Images/29.jpeg",
+          "Images/29.jpeg",
           head:"Indo Era",
           name: "Indo Era Navy Cotton Embroidered Straight Kurta",
-        price: "₹739",
+        price: "739",
         strikedoffprice: "₹2599",
-        rating:"4(6)",
+        rating:"4.0(6)",
       },
       {
         image_url:
-          "/Images/30.jpeg",
+          "Images/30.jpeg",
           head:"Rustorange",
           name: "Rustorange Black & Yellow Striped Long Kurta",
-        price: "₹1399",
+        price: "1399",
         strikedoffprice: "₹2799",
-        rating:null,
+        rating:"",
       },
       {
         image_url:
-          "/Images/31.jpeg",
+          "Images/31.jpeg",
           head:"Indo Era",
           name: "Indo Era Black A Line Kurta",
-        price: "₹1079",
+        price: "1079",
         strikedoffprice: "₹3399",
-        rating:"4(34)",
+        rating:"4.0(34)",
       },
       {
         image_url:
-          "/Images/32.jpeg",
+          "Images/32.jpeg",
           head:"Rustorange",
           name: "Rustorange Black & Green Striped Long Kurta",
-        price: "₹1399",
+        price: "1399",
         strikedoffprice: "₹2799",
         rating:"3.8(6)",
       },
       {
         image_url:
-          "/Images/33.jpeg",
+          "Images/33.jpeg",
           head:"Janasya",
           name: "Janasya Blue Tie-Dye Fit & Flare Kaftan Kurta",
-        price: "₹795",
+        price: "795",
         strikedoffprice: "₹2198",
-        rating:"5(2)",
+        rating:"5.0(2)",
       },
       {
         image_url:
-          "/Images/34.jpeg",
+          "Images/34.jpeg",
           head:"Yuris",
           name: "Yuris Blue Printed Straight Kurta",
-        price: "₹759",
+        price: "759",
         strikedoffprice: "₹1999",
         rating:"3.3(6)", 
       },
       {
         image_url:
-          "/Images/35.jpeg",
+          "Images/35.jpeg",
           head:"Juniper",
           name: "Juniper Navy A Line Kurta",
-        price: "₹775",
+        price: "775",
         strikedoffprice: "₹3099",
-        rating:"5(3)",
+        rating:"5.0(3)",
       },
       {
         image_url:
-          "/Images/36.jpeg",
+          "Images/36.jpeg",
           head:"Janasya",
           name: "Janasya Pewter Printed Straight Kurta",
-        price: "₹492",
+        price: "492",
         strikedoffprice: "₹1199",
         rating:"4.1(10)",
       },
       {
         image_url:
-          "/Images/37.jpeg",
+          "Images/37.jpeg",
           head:"Yuris",
           name: "Yuris Pink Printed A Line Kurta",
-        price: "₹769",
+        price: "769",
         strikedoffprice: "₹1799",
-        rating:"5(6)",
+        rating:"5.0(6)",
       },
       {
         image_url:
-          "/Images/38.jpeg",
+          "Images/38.jpeg",
           head:"W",
           name: "W Baby Pink Cotton Printed Straight Kurti",
-        price: "₹849",
+        price: "849",
         strikedoffprice: "₹1699",
         rating:"4.1(10)",
       },
       {
         image_url:
-          "/Images/39.jpeg",
+          "Images/39.jpeg",
           head:"Janasya",
           name: "Janasya Blue Printed Straight Kurta",
-        price: "₹474 ",
+        price: "474 ",
         strikedoffprice: "₹1399",
-        rating:null,
+        rating:"",
       },
       {
         image_url:
-          "/Images/40.jpeg",
+          "Images/40.jpeg",
           head:"KSUT",
           name: "KSUT Blue & White Cotton Printed Straight Kurta",
-        price: "₹749",
+        price: "749",
         strikedoffprice: "₹1799",
-        rating:"5(1)",
+        rating:"5.0(1)",
       },
   ];
   
@@ -373,7 +373,7 @@ var womensData = [
     var name=document.createElement("h3")
     name.innerText=ele.head;
     var bok=document.createElement("img")
-    bok.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFEQ9Tf-WEBVla9aEPvqAcra5Qk0ygt5Zn8Q&usqp=CAU"
+    bok.src="https://encrypted-tbn0.gstatic.com/image?q=tbn:ANd9GcTFEQ9Tf-WEBVla9aEPvqAcra5Qk0ygt5Zn8Q&usqp=CAU"
     bok.setAttribute("id","bok")
 
     var div2=document.createElement("div")
@@ -386,7 +386,7 @@ var womensData = [
     div3.setAttribute("id","div3")
 
     var pri=document.createElement("p")
-    pri.innerText=ele.price
+    pri.innerText= "₹"+ele.price
 
     var off=document.createElement("s")
     off.innerText=ele.strikedoffprice
@@ -399,7 +399,7 @@ var womensData = [
  
 
     div2.append(name,bok)
-    div3.append(pri)
+    div3.append(pri,off)
     div.append(img,div2,detail,div3,rat)
 
     document.querySelector("#box").append(div);
