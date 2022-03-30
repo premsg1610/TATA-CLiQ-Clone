b4a388950b8479f0fa943bb84d5cc21cf10b5ec0
shipra singh <shiprasingh4796@gmail.com>
2022-03-30 15:16:27 +0530

    womens data

---
+++ women.js
@@ -0,0 +1,360 @@
+var womensData = [
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/1.jpeg",
+      head: "Utsa",
+      name: "Utsa by Westside Greey Printed A-Line Kurti",
+      price: "₹799",
+      strikedoffprice: null,
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/2.jpeg",
+      head:"Aurelia",
+      name: "Aurelia White Cotton Embroidered Straight Kurta",
+      price: "₹749",
+      strikedoffprice: "₹1499",
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/3.jpeg",
+        head:"Utsa",
+        name: "Utsa by Westside Mauve Embroidered A-Line Kurta",
+      price: "₹1299",
+      strikedoffprice: null,
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/4.jpeg",
+        head:"W",
+        name: "W Minion Yellow Embellished A Line Kurta",
+      price: "₹2499",
+      strikedoffprice: "₹4999",
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/5.jpeg",
+        head:"Biba",
+        name: "Biba Green Printed Straight Kurti",
+      price: "₹1599",
+      strikedoffprice: null,
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/6.jpeg",
+        head:"Biba",
+        name: "Biba Indigo Blue Cotton Straight Kurta",
+      price: "₹998",
+      strikedoffprice: "₹1999",
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/7.jpeg",
+        head:"Aurelia",
+        name: "Aurelia Blue Striped Kurta",
+      price: "₹649",
+      strikedoffprice: "₹1299",
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/8.jpeg",
+        head:"Zuba",
+        name: "Zuba by Westside Orange Printed Ombre A-line Kurta",
+      price: "₹2599",
+      strikedoffprice: null,
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/9.jpeg",
+        head:"Zuba",
+        name: "Zuba by Westside Green Floral A-Line Kurta",
+      price: "₹2299",
+      strikedoffprice: null,
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/10.jpeg",
+        head:"Diza",
+        name: "Diza Curves by Westside Orange Pintuck A-Line Kurta",
+      price: "₹1199",
+      strikedoffprice: null,
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/11.jpeg",
+        head:"Fabindia",
+        name: "Fabindia Black Cotton Straight Kurta",
+      price: "₹1399",
+      strikedoffprice: null,
+    },
+    {
+      image_url:
+        "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/12.jpeg",
+        head:"Diza",
+        name: "Diza Curves by Westside Pink Chevron Straight Kurta",
+      price: "₹799",
+      strikedoffprice: null,
+    },
+    {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/13.jpeg",
+          head:"Fabindia",
+          name: "Fabindia Pink Cotton Chequered Straight Kurta",
+        price: "₹1499",
+        strikedoffprice: null,
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/14.jpeg",
+          head:"Global Desi",
+          name: "Global Desi Blue Printed Kurta",
+        price: "₹1329",
+        strikedoffprice: "₹1899",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/15.jpeg",
+          head:"Juniper",
+          name: "Juniper Grey Cotton Embroidered A Line Kurta With Mask",
+        price: "₹1039",
+        strikedoffprice: "₹1999",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/16.jpeg",
+          head:"Juniper",
+          name: "Juniper Green Printed Straight Kurta With Mask",
+        price: "₹799",
+        strikedoffprice: "₹1699",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/17.jpeg",
+          head:"Global Desi",
+          name: "Global Desi Pink Printed Kurta",
+        price: "₹2099",
+        strikedoffprice: null,
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/18.jpeg",
+          head:"Aks",
+          name: "Aks Coral Cotton Printed Straight Kurta",
+        price: "₹499",
+        strikedoffprice: "₹999",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/19.jpeg",
+          head:"Anubhutee",
+          name: "Anubhutee Blue Printed Straight Kurti",
+        price: "₹584",
+        strikedoffprice: "₹1299",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/20.jpeg",
+          head:"Aks",
+          name: "Aks Grey Printed Straight Kurta",
+        price: "₹849",
+        strikedoffprice: "₹1699",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/21.jpeg",
+          head:"Soch",
+          name: "Soch Grey & White Embellished A Line Kurta",
+        price: "₹999",
+        strikedoffprice: "₹1998",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/22.jpeg",
+          head:"Jaipur Kurti",
+          name: "Jaipur Kurti Yellow Cotton Printed Straight Kurta",
+        price: "₹542",
+        strikedoffprice: "₹1349",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/23.jpeg",
+          head:"Gerua",
+          name: "Gerua Beige & Red Printed A Line Kurti",
+        price: "₹674",
+        strikedoffprice: "₹1499",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/24.jpeg",
+          head:"Yufta",
+          name: "Yufta Women Blue & Offwhite Printed Kaftan Kurta",
+        price: "₹799",
+        strikedoffprice: "₹1999",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/25.jpeg",
+          head:"Gerua",
+          name: "Gerua Yellow Printed Cotton Kurti",
+        price: "₹494",
+        strikedoffprice: "₹1099",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/26.jpeg",
+          head:"Tara-C-Tara",
+          name: "Tara-C-Tara Pink Embellished A Line Kurta With Dupatta",
+        price: "₹2759",
+        strikedoffprice: "₹4599",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/27.jpeg",
+          head: "Tara-C-Tara",
+          name: "Tara-C-Tara Light Blue V Neck Anarkali Kurta With Dupatta",
+        price: "₹2579",
+        strikedoffprice: "₹4299",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/28.jpeg",
+          head:"Yufta",
+          name: "Yufta Multicolor Printed Kaftan Kurti",
+        price: "₹719",
+        strikedoffprice: "₹1799",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/29.jpeg",
+          head:"Indo Era",
+          name: "Indo Era Navy Cotton Embroidered Straight Kurta",
+        price: "₹739",
+        strikedoffprice: "₹2599",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/30.jpeg",
+          head:"Rustorange",
+          name: "Rustorange Black & Yellow Striped Long Kurta",
+        price: "₹1399",
+        strikedoffprice: "₹2799",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/31.jpeg",
+          head:"Indo Era",
+          name: "Indo Era Black A Line Kurta",
+        price: "₹1079",
+        strikedoffprice: "₹3399",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/32.jpeg",
+          head:"Rustorange",
+          name: "Rustorange Black & Green Striped Long Kurta",
+        price: "₹1399",
+        strikedoffprice: "₹2799",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/33.jpeg",
+          head:"Janasya",
+          name: "Janasya Blue Tie-Dye Fit & Flare Kaftan Kurta",
+        price: "₹795",
+        strikedoffprice: "₹2198",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/34.jpeg",
+          head:"Yuris",
+          name: "Yuris Blue Printed Straight Kurta",
+        price: "₹759",
+        strikedoffprice: "₹1999",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/35.jpeg",
+          head:"Juniper",
+          name: "Juniper Navy A Line Kurta",
+        price: "₹775",
+        strikedoffprice: "₹3099",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/36.jpeg",
+          head:"Janasya",
+          name: "Janasya Pewter Printed Straight Kurta",
+        price: "₹492",
+        strikedoffprice: "₹1199",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/37.jpeg",
+          head:"Yuris",
+          name: "Yuris Pink Printed A Line Kurta",
+        price: "₹769",
+        strikedoffprice: "₹1799",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/38.jpeg",
+          head:"W",
+          name: "W Baby Pink Cotton Printed Straight Kurti",
+        price: "₹849",
+        strikedoffprice: "₹1699",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/39.jpeg",
+          head:"Janasya",
+          name: "Janasya Blue Printed Straight Kurta",
+        price: "₹474 ",
+        strikedoffprice: "₹1399",
+      },
+      {
+        image_url:
+          "C:/Users/Pramod.Singh/Desktop/shipra/HTML MASAI/UNIT-2 PROJECT/Unit-2-Project---Tata-Cliq/images/40.jpeg",
+          head:"KSUT",
+          name: "KSUT Blue & White Cotton Printed Straight Kurta",
+        price: "₹749",
+        strikedoffprice: "₹1799",
+      },
+  ];
+  
+  womensData.map(function(elem){
+      var box = document.createElement("div");
+      box.setAttribute("class","bag");
+  
+      var imgDiv = document.createElement("div");
+      imgDiv.setAttribute("class","imgDiv");
+
+      var img = document.createElement("img");
+      img.src = elem.image_url;
+
+      imgDiv.append(img);
+
+      var head = document.createElement("h4");
+      head.innerText = elem.head;
+      head.setAttribute("class","head");
+
+      var name = document.createElement("p");
+      name.textContent = elem.name;
+      name.setAttribute("class","para");
+
+      var priceDiv = document.createElement("div");
+      priceDiv.setAttribute("class","innerDiv");
+  
+      var price = document.createElement("p");
+      price.innerText = elem.price;
+      price.style.fontWeight = "bold"
+  
+      var strikedoffprice = document.createElement("p");
+      strikedoffprice.innerText = elem.strikedoffprice; 
+      strikedoffprice.style.textDecoration = "line-through";
+      strikedoffprice.style.fontSize="14px"
+  
+      priceDiv.append(price,strikedoffprice);
+  
+      box.append(imgDiv,head,name,priceDiv);
+      document.querySelector("#product").append(box);
+  });
