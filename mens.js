063a89ed7a7252b19fc5e3b696e703d226aaa387
krashoka <kumarashoka7677@gmail.com>
2022-03-30 21:56:37 +0530

    Shoes Striked price added

--- mens.js
+++ mens.js
@@ -5,6 +5,7 @@ var mensData =  [
         nam : "Red Chief" ,
         namefull: "Red Chief Men's Tan Casual Shoes" ,
         price :  "₹3695" ,
+        off : "₹4695",
         rating : "4.3"
 
     },
@@ -12,6 +13,7 @@ var mensData =  [
         nam : "SOLEPLAY",
         namefull: "SOLEPLAY by Westside Beige Rainwear Loafers" ,
         price :  " ₹799" ,
+        off :  " ₹999" ,
         rating : " 4.4 (8)"
 
     },
@@ -19,6 +21,7 @@ var mensData =  [
         nam : "Mochi" ,
         namefull: " Mochi Men's Tan Casual Loafers" ,
         price :  " ₹1743" ,
+        off :  " ₹2743" ,
         rating : "4 (1)"
 
     },
@@ -26,6 +29,7 @@ var mensData =  [
         nam : "U.S. Polo Assn." ,
         namefull: "  U.S. Polo Assn. Men's Quebec Black Loafers" ,
         price :  " ₹2398" ,
+        off :  " ₹3998" ,
         rating : "5(1)"
 
     },
@@ -33,6 +37,7 @@ var mensData =  [
         nam : "SOLEPLAY",
         namefull: " SOLEPLAY by Westside Indigo Perforated Loafers" ,
         price :  " ₹799" ,
+        off :  " ₹1199" ,
         rating : "3.8(147)"
 
     },
@@ -40,6 +45,7 @@ var mensData =  [
         nam : "Florsheim" ,
         namefull: "  Florsheim Brown Casual Loafers" ,
         price :  "₹1996" ,
+        off :  "₹2996" ,
         rating : "4.6(5)"
 
     },
@@ -47,6 +53,7 @@ var mensData =  [
         nam : "Ruosh" ,
         namefull: "  Ruosh Men's Tan Casual Loafers" ,
         price :  " ₹4990" ,
+        off :  " ₹6990" ,
         rating : "4"
 
     },
@@ -54,6 +61,7 @@ var mensData =  [
         nam : "Skechers" ,
         namefull: "  Skechers Men's FLEX ADVANTAGE Casual Shoe Slip-Ons" ,
         price :  "₹2400" ,
+        off :  "₹3400" ,
         rating : "3.9(21)"
 
     },
@@ -61,6 +69,7 @@ var mensData =  [
         nam : "Alberto Torresi" ,
         namefull: "  Alberto Torresi Men's Calabria Tan Casual Loafers" ,
         price :  " ₹1999" ,
+        off :  " ₹2999" ,
         rating : "3.1(10)"
 
     },
@@ -68,6 +77,7 @@ var mensData =  [
         nam : "Woodland" ,
         namefull: "  Woodland Men's Khaki Casual Shoes" ,
         price :  "  ₹2577" ,
+        off :  "  ₹4577" ,
         rating : "3.8(4)"
 
     },
@@ -75,6 +85,7 @@ var mensData =  [
         nam : "Clarks" ,
         namefull: "  Clarks Men's Black Boat Shoes" ,
         price :  "₹3899" ,
+        off :  "₹4899" ,
         rating : "3"
 
     },
@@ -82,6 +93,7 @@ var mensData =  [
         nam : "ID" ,
         namefull: " ID Men's Black Casual Loafers" ,
         price :  " ₹1350" ,
+        off :  " ₹3350" ,
         rating : "3.9(13)"
 
     },
@@ -89,6 +101,7 @@ var mensData =  [
         nam : "Red Tape" ,
         namefull: " Red tape Men's Black Casual Loafers" ,
         price :  "  ₹1474" ,
+        off :  "  ₹3474" ,
         rating : "3.5(81)"
 
     },
@@ -96,6 +109,7 @@ var mensData =  [
         nam : "San Frissco" ,
         namefull: " San Frissco Men's Carbon Black Casual Loafers" ,
         price :  "  ₹1848" ,
+        off :  "  ₹3848" ,
         rating : "5(4)"
 
     },
@@ -104,6 +118,7 @@ var mensData =  [
         nam : "Ruosh" ,
         namefull: " Ruosh Men's Tan Casual Loafers" ,
         price :  "  ₹3990" ,
+        off :  "  ₹5990" ,
         rating : "4(5)"
 
     },
@@ -111,7 +126,8 @@ var mensData =  [
         nam : " Alberto Torresi" ,
         namefull: " Alberto Torresi Men's Men's Maroon Casual Slip-Ons" ,
         price :  "₹1799" ,
-        rating : "4(9)"
+        off :  "₹3799" ,
+        rating : "4(5)"
 
     },
    
@@ -119,6 +135,7 @@ var mensData =  [
         nam : "Aldo" ,
         namefull: " Aldo Men's Cognac Casual Loafers" ,
         price :  " ₹11199" ,
+        off :  " ₹13199" ,
         rating : ""
 
     },
@@ -126,6 +143,7 @@ var mensData =  [
         nam : "Tresmode " ,
         namefull: " Tresmode Men's Brown Casual Loafer" ,
         price :  " ₹4500" ,
+        off :  " ₹6500" ,
         rating : ""
 
     },
@@ -133,6 +151,7 @@ var mensData =  [
         nam : "San Frissco " ,
         namefull: " San Frissco Black Mule Shoes" ,
         price :  "₹1698" ,
+        off :  "₹2698" ,
         rating : " 4.4(9)"
 
     },
@@ -140,8 +159,168 @@ var mensData =  [
         nam : "Lee Cooper" ,
         namefull: "  Lee Cooper Men's CORE EVOLUTION Tan Loafers" ,
         price :  "   ₹2549" ,
+        off :  "   ₹4549" ,
         rating : ""
     },
+
+    {  image_url : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSelKUzPIhZYrwGfNQ23BaLLl1DYSRx7cHqBPQtmXxfHn-Suh4u" ,
+        nam : "Hush Puppies" ,
+        namefull: "  Hush Puppies by Bata Men's Tan Casual Loafers" ,
+        price :  "    ₹3399" ,
+        off :  "   ₹3999" ,
+        rating : ""
+    },
+
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQneEYMuUFF8InOLE9r1aiNjl6BG4xtBIVMb4PuLByKzqNXa2vz" ,
+        nam : "Woodland" ,
+        namefull: " Woodland Men's Tobacco Casual Slip-Ons" ,
+        price :  "    ₹2621" ,
+        off :  "   ₹3495" ,
+        rating : "3.3(9)"
+    },
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRo1ryKGdk6eUKBEgt9fIh-TwAaYzii8DE7L1bfaNJy-D2Pqsk9" ,
+        nam : "Bata" ,
+        namefull: "Bata Men's Black Casual Slip-Ons" ,
+        price :  "    ₹906" ,
+        off :  "   ₹1066" ,
+        rating : "4(1)"
+    },
+
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTe2g0WFZjfABt9VyQpJ6bf9E9y6G-fDmhNanZYxkkSvLmiu1tf" ,
+        nam : "Pravers England" ,
+        namefull: " Pavers England Men's Navy Plimsolls" ,
+        price :  "    ₹4999" ,
+        off :  "" ,
+        rating : "5(1)"
+    },
+
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ45SXSupNjZnd5EYgOZk5b1tCnggvSYidXkQz2H6OijGlq-jWK" ,
+        nam : "Woodland" ,
+        namefull: "Woodland Men's Brown Casual Slip-Ons " ,
+        price :  "₹2795" ,
+        off :  "" ,
+        rating : ""
+    },
+
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSEIhjFXsTMyAS8HR9FTDEacBCM40V9UE0AD-ip91VU_7A5mrDK" ,
+        nam : "Sparx" ,
+        namefull: "Sparx Black Casual Plimsolls" ,
+        price :  "₹499" ,
+        off :  "" ,
+        rating : "4.2(74)"
+    },
+
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR2fsCb4KNNlGxu1TRytLgUD8ysZJobGC0dsL_Bx4v-6yFHTjXn" ,
+        nam : "Bugatti" ,
+        namefull: "Bugatti Men's Chocolate Brown Mule Shoes" ,
+        price :  "₹7499" ,
+        off :  "" ,
+        rating : ""
+    },
+
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQukuJF96G_VYofy7PRwSG-MP9a9kkcADyZuoDOxLTTqtaEh97F" ,
+        nam : "Red Chief" ,
+        namefull: "Red Chief Men's Navy/Blue Casual Shoes" ,
+        price :  "    ₹1999" ,
+        off :  "   ₹3999" ,
+        rating : "4.2(152)"
+    },
+
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTWv_MhHBRmEx7G_OMtuN0Gk85Ed7iobRb4sto3G_UCcyd-s_qy" ,
+        nam : "Woodland" ,
+        namefull: "Sparx Black Casual Plimsolls" ,
+        price :  "    ₹521" ,
+        off :  "   ₹549" ,
+        rating : "3.9(54)"
+    },
+
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnRm2Da7udA1PKkS6X09mW_AjLvxFwMjvX9zsXiCsDht5zdIZf" ,
+        nam : "Puma" ,
+        namefull: "Puma Men's Unisex Lazy Knit Peacoat Slip-Ons" ,
+        price :  "    ₹1949" ,
+        off :  "   ₹2999" ,
+        rating : "4(53)"
+    },
+
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTBUvpc9EdYJy1MXg3oGVDqYRo5PKa3Fr5ibC04ioSGWFx5Ddkm" ,
+        nam : "Pavers England" ,
+        namefull: "Pavers England Men's Brown Plimsolls" ,
+        price :  "    ₹4999" ,
+        off :  "" ,
+        rating : "5(1)"
+    },
+
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQgP55WsnK-5iCYz47T4KfsC_K-IytrzcZuhF_df4cgt1eFNsCO" ,
+        nam : "Red Chief" ,
+        namefull: "Red Chief Men's Tan Casual Slip-Ons" ,
+        price :  "    ₹2579" ,
+        off :  "   ₹4299" ,
+        rating : "4(19)"
+    },
+
+    {  image_url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSybd27cQTfaLXBvSo6rZvMAjheZ8vrYv5Nqn4Vz8_A8DwDXfiu" ,
+        nam : "Allen Solly" ,
+        namefull: " Allen Solly Men's Brown Casual Loafers" ,
+        price :  "    ₹2079" ,
+        off :  "   ₹3199" ,
+        rating : ""
+    },
+
+    {  image_url : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ9laOfmdB_95Lxf5we0qmMTz8qcxFjlL6KIfX0IVEPTc2_HCJg" ,
+        nam : "Bugatti" ,
+        namefull: "Bugatti Men's Navy Casual Slip-Ons" ,
+        price :  "₹8499" ,
+        off :  "" ,
+        rating : ""
+    },
+
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq-AQVdu1ByU07LjntyktxGqoB9jAZ_eBCA9_-2UV158TNkn_B" ,
+        nam : "Pavers England" ,
+        namefull: "Pavers England Men's Black Fisherman Sandals" ,
+        price :  "₹3999" ,
+        off :  "" ,
+        rating : "4.7(3)"
+    },
+
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRoteYJ7l6oeeFB4b0BN50zgjzbfx4Q0Y0rnsPyp-kS3B6iZSQc" ,
+        nam : "Arrow" ,
+        namefull: "Arrow Men's Carmine Tan Slip-Ons" ,
+        price :  "    ₹2598" ,
+        off :  "   ₹3999" ,
+        rating : "3.7(3)"
+    },
+
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6ik-OBf8JGwnJjtm20eFUgWA3-cAa48nm3WMujogt9hcb2haN" ,
+        nam : "Allen Cooper" ,
+        namefull: "Allen Cooper Men's Brown Casual Loafers" ,
+        price :  "    ₹1760" ,
+        off :  "   ₹5499" ,
+        rating : "3.3(9)"
+    },
+
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQiWwK65ErUK4LDroO74EDXlv_4jbeyAuSn4RKpxFpxxwfB3nsw" ,
+        nam : "Red Chief" ,
+        namefull: "Red Chief Men's Camel Derby Shoes" ,
+        price :  "    ₹2579" ,
+        off :  "   ₹4299" ,
+        rating : "4.3(3)"
+    },
+
+    {  image_url : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ7jANyZWv2LYxUudNzZmixet99VoW6vSEB1Y9jnKi-Iey39OaD" ,
+        nam : "Allen Cooper" ,
+        namefull: "Allen Cooper Men's Black Casual Loafers" ,
+        price :  "    ₹1886" ,
+        off :  "   ₹5240" ,
+        rating : ""
+    },
+
+    {  image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQomv9FEqPX-7op8oU9Chl-GmnCudUj-zIbJE7KYwKaT3VFLe6V" ,
+        nam : "SOLEPLAY" ,
+        namefull: "SOLEPLAY by Westside Grey Perforated Plimsolls" ,
+        price :  "    ₹799" ,
+        off :  "" ,
+        rating : "3.9(151)"
+    }
     ];
 
     mensData.map(function(ele){
@@ -165,20 +344,23 @@ var mensData =  [
     
         var div3=document.createElement("div")
         div3.setAttribute("id","div3")
+        div3.style.display="flex"
     
         var pri=document.createElement("p")
         pri.innerText=ele.price
-        // var off=document.createElement("s")
-        // off.innerText=ele.off
+        var off=document.createElement("s")
+        off.innerText=ele.off
+        off.style.paddingLeft = "15px"
         var rat=document.createElement("p")
         rat.innerText=ele.rating
+        rat.style.color = "green"
          
     
         // "<i class="fa-light fa-bookmark"></i>"
      
     
         div2.append(name,bok)
-        div3.append(pri)
+        div3.append(pri,off)
         div.append(img,div2,detail,div3,rat)
     
         document.querySelector("#box").append(div);
